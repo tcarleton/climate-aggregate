3a81e736c8dd384a734b08c69b9e08e1d4c0cb9d
Sara Orofino <saraorofino@emLab-iMac-Pro.local>
2022-03-23 16:47:29 -0700

    Add input polygon name to list of inputs

--- code/run.R
+++ code/run.R
@@ -5,11 +5,12 @@ source(here::here('code', '01_spatial_overlay.R'))
 source(here::here('code', '02_agg_climate_data.R'))
 
 ## Load other required data (i.e. input polygons)
-us_counties <- tigris::counties()
+input_polygons <- tigris::counties()
 
 ## Inputs - update as necessary 
 # Note: input polygons and input_polygon_name vars have to be entered directly into the functions
 climate_data <- 'era5'
+input_polygons_name <- 'us_counties'
 id_var <- 'GEOID' #col name that uniquely identifies each polygon 
 years <- 2000:2020
 climate_variable <- 'prcp'
@@ -26,24 +27,15 @@ if(both_steps){
   
   # Edit input polygons 
   calc_geoweights(data_source = climate_data,
-                  input_polygons = us_counties,
+                  input_polygons = input_polygons,
                   polygon_id = id_var)
   
-  # Edit input_polygon_names
-  agg_climate_data_multiyear(years = years,
-                             data_source = climate_data,
-                             climate_var = climate_variable,
-                             input_polygons_name = 'us_counties',
-                             trans = trans,
-                             trans_specs = trans_specs)
+
+} 
   
-} else {
-  
-  # Edit input_polygon_names
-  agg_climate_data_multiyear(years = years,
-                             data_source = climate_data,
-                             climate_var = climate_variable,
-                             input_polygons_name = 'us_counties',
-                             trans = trans,
-                             trans_specs = trans_specs)
-}
+# Edit input_polygon_names
+agg_climate_data_multiyear(years = years,
+                           data_source = climate_data,
+                           climate_var = climate_variable,
+                           trans = trans,
+                           trans_specs = trans_specs)
