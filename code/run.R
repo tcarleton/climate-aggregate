dd48f409fec232bf505602b9e992b238eeb5dcf9
Sara Orofino <saraorofino@bren.ucsb.edu>
2022-05-16 10:51:06 -0700

    Test pipeline with precipitation variable

--- code/run.R
+++ code/run.R
@@ -16,7 +16,7 @@ input_polygons <- read_sf(file.path(here::here(), "data", "shapefiles", "tl_2019
 ## Inputs - update as necessary 
 # Defined - must match one of the options
 climate_data <- 'era5' # era5 is only option for now
-climate_variable <- 'temp' # temp, prcp, or uv
+climate_variable <- 'prcp' # temp, prcp, or uv
 years <- 2005:2010 # any sequence of years 2002:2020
 trans <- 'polynomial' # polynomial is only option for now
 trans_specs <- 3 # Specs must match the trans type, numeric when trans=polynomial
@@ -32,7 +32,7 @@ weights_name <- 'era5_cropland_2003_full' # The name of the secondary weights fi
 ## Steps to run 
 # Specify if you need to run step 1 and step 2
 # If TRUE runs both, if FALSE runs step 2 only 
-both_steps <- TRUE
+both_steps <- FALSE
 
 # both_steps = T run both, else run step 2 only 
 if(both_steps){
