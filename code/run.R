23bf475b35c552a0b822f63c28e9f8cad5747bc6
Tracey Mangin <traceymangin@Traceys-MacBook-Pro.local>
2022-06-27 14:43:27 -0700

    udpate inputs for prcp JPN

--- code/run.R
+++ code/run.R
@@ -11,22 +11,25 @@ source(here::here('code', '01_spatial_overlay.R'))
 source(here::here('code', '02_agg_climate_data.R'))
 
 ## Load other required data (i.e. input polygons)
-# Testing with CA counties
-input_polygons <- read_sf(file.path(root_dir, "data", "shapefiles", "tl_2019_us_county", "tl_2019_us_county.shp")) %>% dplyr::filter(STATEFP == '06')
+
+# # Testing with CA counties
+# input_polygons <- read_sf(file.path(root_dir, "data", "shapefiles", "tl_2019_us_county", "tl_2019_us_county.shp")) %>% dplyr::filter(STATEFP == '06')
+
+input_polygons <- read_sf(file.path(root_dir, "data", "shapefiles", "JPN", "JPN_adm1.shp"))
 
 ## Inputs - update as necessary 
 # Defined - must match one of the options
 climate_data <- 'era5' # era5 is only option for now
 climate_variable <- 'prcp' # temp, prcp, or uv
 daily_agg <- 'sum' # average or sum; method for aggregating from hourly to daily 
-years <- 2005:2010 # any sequence of years 2002:2020
+years <- 2009:2018 # any sequence of years 2002:2020
 trans <- 'polynomial' # polynomial is only option for now
-trans_specs <- 3 # Specs must match the trans type, numeric when trans=polynomial
-id_var <- 'GEOID' # Col name in the input_polygons shp that uniquely identifies each polygon 
+trans_specs <- 3 # Specs must match the trans type, numeric when trans=polynomial (5 for temp, 3 for prcp)
+id_var <- 'ID_1' # Col name in the input_polygons shp that uniquely identifies each polygon 
 weights <- TRUE # True to define a second set of weights, FALSE to use only area weights
 
 # Flexible - not limited to specific options
-input_polygons_name <- 'ca_counties' # Name used in saving function outputs; should relate to the polygons used
+input_polygons_name <- 'jpn_prefecture' # Name used in saving function outputs; should relate to the polygons used
 weights_type <- 'crop' # Added to the name of the output file to indicate what secondary weights are used 
 weights_name <- 'era5_cropland_2003_full' # The name of the secondary weights file, cannot be blank if weights = TRUE
 
@@ -34,7 +37,7 @@ weights_name <- 'era5_cropland_2003_full' # The name of the secondary weights fi
 ## Steps to run 
 # Specify if you need to run step 1 and step 2
 # If TRUE runs both, if FALSE runs step 2 only 
-both_steps <- FALSE
+both_steps <- TRUE
 
 # both_steps = T run both, else run step 2 only 
 if(both_steps){
