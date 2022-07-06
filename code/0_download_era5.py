a6a1a8cd9cf3727ac7e4b3dd118d7db654dc884c
traceybit <traceymangin@gmail.com>
2022-07-06 15:45:47 -0700

    update years

--- code/0_download_era5.py
+++ code/0_download_era5.py
@@ -92,5 +92,5 @@ for v in new_varnames:
     
 # download data 2002-2021 for each variable
 for i in range(len(variables)):
-    for y in range(1994, 1998):
+    for y in range(1990, 1995):
         retrieve(y, variables[i], new_varnames[i])
