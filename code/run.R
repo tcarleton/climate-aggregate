91625190f269851daa2e92022fb508a0a3ea5333
Tracey Mangin <traceymangin@Traceys-MacBook-Pro.local>
2022-07-16 21:54:51 -0400

    CHL prcp

--- code/run.R
+++ code/run.R
@@ -15,21 +15,21 @@ source(here::here('code', '02_agg_climate_data.R'))
 # # Testing with CA counties
 # input_polygons <- read_sf(file.path(root_dir, "data", "shapefiles", "tl_2019_us_county", "tl_2019_us_county.shp")) %>% dplyr::filter(STATEFP == '06')
 
-input_polygons <- read_sf(file.path(input_dir, "data", "shapefiles", "MEX", "boundaries_4795_IPUMS_MX_HSLAD_1990_2015.shp"))
+input_polygons <- read_sf(file.path(input_dir, "data", "shapefiles", "CHL", "cl_comunas_geo_mortality_adm2.shp"))
 
 ## Inputs - update as necessary 
 # Defined - must match one of the options
 climate_data <- 'era5' # era5 is only option for now
 climate_variable <- 'prcp' # temp, prcp, or uv
 daily_agg <- 'sum' # average or sum; method for aggregating from hourly to daily: use average for temp, sum for prcp
-years <- 1990:2018 # any sequence of years 1967:2020
+years <- 1997:2013 # any sequence of years 1967:2020
 trans <- 'polynomial' # polynomial is only option for now
 trans_specs <- 3 # Specs must match the trans type, numeric when trans=polynomial (5 for temp, 3 for prcp)
-id_var <- 'GEOID' # Col name in the input_polygons shp that uniquely identifies each polygon 
+id_var <- 'adm2_id' # Col name in the input_polygons shp that uniquely identifies each polygon 
 weights <- TRUE # True to define a second set of weights, FALSE to use only area weights
 
 # Flexible - not limited to specific options
-input_polygons_name <- 'mex_area' # Name used in saving function outputs; should relate to the polygons used
+input_polygons_name <- 'chl_area' # Name used in saving function outputs; should relate to the polygons used
 weights_type <- 'crop' # Added to the name of the output file to indicate what secondary weights are used 
 weights_name <- 'era5_cropland_2003_full' # The name of the secondary weights file, cannot be blank if weights = TRUE
 
@@ -37,7 +37,7 @@ weights_name <- 'era5_cropland_2003_full' # The name of the secondary weights fi
 ## Steps to run 
 # Specify if you need to run step 1 and step 2
 # If TRUE runs both, if FALSE runs step 2 only 
-both_steps <- FALSE
+both_steps <- TRUE
 
 # both_steps = T run both, else run step 2 only 
 if(both_steps){
