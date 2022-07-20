fcd9d1334524064e9e528ac7807d8fe58b225d10
traceybit <traceymangin@gmail.com>
2022-07-20 16:48:32 -0700

    try running usa temp on all years

--- code/run.R
+++ code/run.R
@@ -20,11 +20,11 @@ input_polygons <- read_sf(file.path(input_dir, "data", "shapefiles", "USA", "tl_
 ## Inputs - update as necessary 
 # Defined - must match one of the options
 climate_data <- 'era5' # era5 is only option for now
-climate_variable <- 'prcp' # temp, prcp, or uv
-daily_agg <- 'sum' # average or sum; method for aggregating from hourly to daily: use average for temp, sum for prcp
-years <- 1991:2004 # any sequence of years 1967:2020
+climate_variable <- 'temp' # temp, prcp, or uv
+daily_agg <- 'average' # average or sum; method for aggregating from hourly to daily: use average for temp, sum for prcp
+years <- 1968:2004 # any sequence of years 1967:2020
 trans <- 'polynomial' # polynomial is only option for now
-trans_specs <- 3 # Specs must match the trans type, numeric when trans=polynomial (5 for temp, 3 for prcp)
+trans_specs <- 5 # Specs must match the trans type, numeric when trans=polynomial (5 for temp, 3 for prcp)
 id_var <- 'GEOID' # Col name in the input_polygons shp that uniquely identifies each polygon 
 weights <- TRUE # True to define a second set of weights, FALSE to use only area weights
 
