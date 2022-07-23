4f78e9aebffd39bcab565f07b3c2b49f9f17ebc9
traceybit <traceymangin@gmail.com>
2022-07-22 17:57:44 -0700

    next batch

--- code/combine_outputs.R
+++ code/combine_outputs.R
@@ -16,5 +16,8 @@ usa_prcp2 <- fread(paste0(output_path, 'usa_area_era5_prcp_sum_1971_1980_polynom
 usa_prcp3 <- fread(paste0(output_path, 'usa_area_era5_prcp_sum_1981_1990_polynomial_3_area_crop_weights.csv'))
 usa_prcp4 <- fread(paste0(output_path, 'usa_area_era5_prcp_sum_1991_2004_polynomial_3_area_crop_weights.csv'))
 
-## read in files
+## bind
+usa_prcp <- rbind(usa_prcp1, usa_prcp2, usa_prcp3, usa_prcp4)
 
+## save
+fwrite(usa_prcp, file = paste0(output_path, 'usa_area_era5_prcp_sum_1968_2004_polynomial_3_area_crop_weights.csv'))
