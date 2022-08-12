1ff3533df185413fedd7bcfafa2d768fe4e5d39c
Tracey Mangin <traceymangin@Traceys-MacBook-Pro.local>
2022-08-12 10:12:33 -0700

    eu run

--- code/run.R
+++ code/run.R
@@ -22,14 +22,14 @@ input_polygons <- read_sf(file.path(input_dir, "data", "shapefiles", "EU", "NUTS
 climate_data <- 'era5' # era5 is only option for now
 climate_variable <- 'temp' # temp, prcp, or uv
 daily_agg <- 'average' # average or sum; method for aggregating from hourly to daily: use average for temp, sum for prcp
-years <- 2001:2010 # any sequence of years 1967:2020
+years <- 1994:2000 # any sequence of years 1967:2020
 trans <- 'polynomial' # polynomial is only option for now
 trans_specs <- 5 # Specs must match the trans type, numeric when trans=polynomial (5 for temp, 3 for prcp)
 id_var <- 'NUTS_ID' # Col name in the input_polygons shp that uniquely identifies each polygon 
 weights <- TRUE # True to define a second set of weights, FALSE to use only area weights
 
 # Flexible - not limited to specific options
-input_polygons_name <- 'nuts2' # Name used in saving function outputs; should relate to the polygons used
+input_polygons_name <- 'eu_nuts2' # Name used in saving function outputs; should relate to the polygons used
 weights_type <- 'crop' # Added to the name of the output file to indicate what secondary weights are used 
 weights_name <- 'era5_cropland_2003_full' # The name of the secondary weights file, cannot be blank if weights = TRUE
 
@@ -37,7 +37,7 @@ weights_name <- 'era5_cropland_2003_full' # The name of the secondary weights fi
 ## Steps to run 
 # Specify if you need to run step 1 and step 2
 # If TRUE runs both, if FALSE runs step 2 only 
-both_steps <- FALSE
+both_steps <- TRUE
 
 # both_steps = T run both, else run step 2 only 
 if(both_steps){
