be2d0b1cd89371873b6af97965043639e2186ff3
traceybit <traceymangin@gmail.com>
2022-08-24 22:34:54 -0700

    run again for chile

--- code/test-package/test_package_run.R
+++ code/test-package/test_package_run.R
@@ -13,57 +13,15 @@ library(raster)
 library(data.table)
 library(rgdal)
 library(parallel)
-# library(ncdf4)
 
 ## set paths
-# root_dir <- file.path("/home/traceymangin/climate-aggregate")
 input_dir <- file.path("/home/tcarleton/Climate")
 save_dir <- file.path("/home/traceymangin")
 
 ## for saving outputs
 ## ---------------------------------------------------------------
 
-data_source <- 'era5'
-sec_weights <- TRUE
-weight_type <- 'area_crop'
-country_name <- 'new_zealand'
-
-## set inputs for run
-## ----------------------------------------------------------------
-
-## secondardy weights
-sec_weight <- cropland_world_2003_era5 ## cropland_world_2003_era5 = crops, pop_world_2015_era5 = pop
-
-## polygon
-input_polygons <- read_sf(file.path(input_dir, "data", "shapefiles", "NZL", "gadm36_NZL_1.shp"))
-
-## polygon id
-polygon_id <- 'NAME_1'
-
-## years
-years <- c(2009:2020)
-  
-## Step 1: filter weights for polygon extent
-## -----------------------------------------------------
-
-polygon_extent <- extent(input_polygons)
-
-sec_weight_filt <- dplyr::filter(sec_weight, 
-                            x >= round(polygon_extent@xmin) - 1, 
-                            x <= round(polygon_extent@xmax) + 1, 
-                            y >= round(polygon_extent@ymin) - 1,
-                            y <= round(polygon_extent@ymax) + 1)
-
-## Step 2: Overlay administrative regions onto your data's grid
-## ------------------------------------------------------------
-
-polygon_weights <- overlay_weights(polygons = input_polygons,
-                                   polygon_id_col = polygon_id,
-                                   grid = era5_grid,
-                                   secondary_weights = sec_weight_filt)
-
-## save weight output
-
+## define paths and create folders for saving weights 
 weights_save_path <- file.path(save_dir, 'climate-out', 'weights')
 
 if(!dir.exists(weights_save_path)){
@@ -74,12 +32,63 @@ if(!dir.exists(weights_save_path)){
   
 }
 
+## define paths and create folders for saving outputs 
+output_save_path <- file.path(save_dir, "climate-out", "output")  
+
+# Check if there is already an output folder
+if(!dir.exists(output_save_path)){
+  
+  # If no - create it
+  message(crayon::yellow('Creating climate-out/output'))
+  dir.create(file.path(save_dir, "climate-out", "output"), recursive=T)
+  
+}
+
+## for naming files
+data_source <- 'era5'
+sec_weights <- TRUE
+weight_type <- 'area_crop'
+country_name <- 'chile'
+
+## set inputs for run
+## ----------------------------------------------------------------
+
+## secondardy weights
+sec_weight <- cropland_world_2003_era5 ## cropland_world_2003_era5 = crops, pop_world_2015_era5 = pop
+
+## polygon
+input_polygons <- read_sf(file.path(input_dir, "data", "shapefiles", "CHL", "cl_comunas_geo_mortality_adm2.shp"))
+
+## polygon id
+polygon_id <- 'adm2_id'
+
+## years
+years <- c(1997:2013)
+  
+## Step 1: filter weights for polygon extent
+## -----------------------------------------------------
+polygon_extent <- extent(input_polygons)
+
+sec_weight_filt <- dplyr::filter(sec_weight, 
+                            x >= round(polygon_extent@xmin) - 1, 
+                            x <= round(polygon_extent@xmax) + 1, 
+                            y >= round(polygon_extent@ymin) - 1,
+                            y <= round(polygon_extent@ymax) + 1)
+
+## Step 2: Overlay administrative regions onto your data's grid
+## ------------------------------------------------------------
+polygon_weights <- overlay_weights(polygons = input_polygons,
+                                   polygon_id_col = polygon_id,
+                                   grid = era5_grid,
+                                   secondary_weights = sec_weight_filt)
+
 ## weights save name
 weights_save_name <- paste0(paste(country_name, polygon_id, data_source, weight_type, sep="-"), ".csv")
 
-# Save message
+## save message
 message(crayon::yellow('Saving', weights_save_name, 'to', weights_save_path))
 
+## save file
 fwrite(polygon_weights, file = file.path(weights_save_path, weights_save_name))
 
 
@@ -94,10 +103,10 @@ max_y <- max(polygon_weights$y) + 0.5
 
 weights_ext <- raster::extent(min_x, max_x, min_y, max_y)
 
-## Run staggregate_polynomial function over multiple years in parallel
+## run staggregate_polynomial function over multiple years in parallel
 ## --------------------------------------------------------------------------
 
-## Function for cropping raster and running staggregate
+## function for cropping raster and running staggregate
 
 run_stagg_year_temp <- function(year) {  
     
@@ -152,34 +161,26 @@ stagg_multiyear_temp_all <- data.table::rbindlist(stagg_multiyear_temp)
 stagg_multiyear_prcp_all <- data.table::rbindlist(stagg_multiyear_prcp)
 
 ## save outputs
-
-output_save_path <- file.path(save_dir, "climate-out", "output")  
-
-# Check if there is already an output folder
-  if(!dir.exists(output_save_path)){
-
-    # If no - create it
-    message(crayon::yellow('Creating climate-out/output'))
-    dir.create(file.path(save_dir, "climate-out", "output"), recursive=T)
-
-  }
-
-  save_name_temp <- paste0(paste(country_name, polygon_id, data_source, weight_type,
+save_name_temp <- paste0(paste(country_name, polygon_id, data_source, weight_type,
                               years[1], years[length(years)], 'temp', sep="-"), ".csv")
 
-  save_name_prcp <- paste0(paste(country_name, polygon_id, data_source, weight_type,
+save_name_prcp <- paste0(paste(country_name, polygon_id, data_source, weight_type,
                                  years[1], years[length(years)], 'prcp', sep="-"), ".csv")
   
-  
-  # Save message
-  message(crayon::yellow('Saving', save_name_temp, 'to', output_save_path))
-  fwrite(stagg_multiyear_temp_all, file = file.path(output_save_path, save_name_temp))
-  
-  message(crayon::yellow('Saving', save_name_prcp, 'to', output_save_path))
-  fwrite(stagg_multiyear_prcp_all, file = file.path(output_save_path, save_name_prcp))
+## save message
+message(crayon::yellow('Saving', save_name_temp, 'to', output_save_path))
 
-  # Done
-  message(crayon::green('Done'))
+## save output
+fwrite(stagg_multiyear_temp_all, file = file.path(output_save_path, save_name_temp))
+  
+## save message
+message(crayon::yellow('Saving', save_name_prcp, 'to', output_save_path))
+
+## save output
+fwrite(stagg_multiyear_prcp_all, file = file.path(output_save_path, save_name_prcp))
+
+## fin
+message(crayon::green('fin'))
 
 
 
