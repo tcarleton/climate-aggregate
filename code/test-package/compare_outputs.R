439a2d67db5d9121df3ded8ff9a50fede724c922
traceybit <traceymangin@gmail.com>
2022-08-25 22:47:26 -0700

    add nz

--- code/test-package/compare_outputs.R
+++ code/test-package/compare_outputs.R
@@ -23,6 +23,17 @@ ch_prcp_df <- fread(paste0(main_path, 'output/chl_area_era5_prcp_sum_1997_2013_p
 ch_temp_stagg_df <- fread(paste0(stagg_path, 'outputs/chile-adm2_id-era5-area_crop-1997-2013-temp.csv'))
 ch_prcp_stagg_df <- fread(paste0(stagg_path, 'outputs/chile-adm2_id-era5-area_crop-1997-2013-prcp.csv'))
 
+## NZL: read in files
+nz_weights_df <- fread(paste0(main_path, 'weights/nzl_region_era5_area_crop_weights.csv'))
+nz_weights_stagg_df <- fread(paste0(stagg_path, '/weights/new_zealand-NAME_1-era5-area_crop.csv'))
+
+nz_temp_df <- fread(paste0(main_path, 'output/nzl_region_era5_temp_average_2009_2020_polynomial_5_area_crop_weights.csv'))
+nz_prcp_df <- fread(paste0(main_path, 'output/nzl_region_era5_prcp_sum_2009_2020_polynomial_3_area_crop_weights.csv'))
+
+nz_temp_stagg_df <- fread(paste0(stagg_path, 'outputs/new_zealand-NAME_1-era5-area_crop-2009-2020-temp.csv'))
+nz_prcp_stagg_df <- fread(paste0(stagg_path, 'outputs/new_zealand-NAME_1-era5-area_crop-2009-2020-prcp.csv'))
+
+
 
 ## compare weights
 ## --------------------------------------------------
@@ -56,7 +67,48 @@ prcp0 <- ch_prcp_df %>%
 
 prcp_comp <- ch_prcp_stagg_df %>%
   pivot_longer(names_to = 'order', values_to = 'value_new', order_1:order_3) %>%
-  full_join(temp0) %>%
+  full_join(prcp0) %>%
   mutate(diff = value_new - value0)
+
+## ----------------------------------------------------
+## NZL
+## ----------------------------------------------------
+
+## compare weights
+## --------------------------------------------------
+
+nz_weights0 <- nz_weights_df %>%
+  pivot_longer(names_to = 'type', values_to = 'value0', w_area:weight)
+
+nz_weight_comp <- nz_weights_stagg_df %>%
+  pivot_longer(names_to = 'type', values_to = 'value_new', w_area:weight) %>%
+  full_join(nz_weights0) %>%
+  mutate(diff = value_new - value0)
+## diff is zero
+
+## compare temp
+## --------------------------------------------------
+nz_temp0 <- nz_temp_df %>%
+  mutate(month = as.integer(str_remove(month, 'month_'))) %>%
+  pivot_longer(names_to = 'order', values_to = 'value0', order_1:order_5)
+
+nz_temp_comp <- nz_temp_stagg_df %>%
+  pivot_longer(names_to = 'order', values_to = 'value_new', order_1:order_5) %>%
+  full_join(nz_temp0) %>%
+  mutate(diff = value_new - value0)
+## conversion...
+
+## compare prcp
+## --------------------------------------------------
+nz_prcp0 <- nz_prcp_df %>%
+  mutate(month = as.integer(str_remove(month, 'month_'))) %>%
+  pivot_longer(names_to = 'order', values_to = 'value0', order_1:order_3)
+
+nz_prcp_comp <- nz_prcp_stagg_df %>%
+  pivot_longer(names_to = 'order', values_to = 'value_new', order_1:order_3) %>%
+  full_join(nz_prcp0) %>%
+  mutate(diff = value_new - value0)
+
+
 
 
